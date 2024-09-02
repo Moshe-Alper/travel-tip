172dc6fb2347525be52ef3bd6906d6b427345fae
Moshik Alper <moshikalper@gmail.com>
2024-09-02 17:14:59 +0300

    Add confirmation when remove location

--- js/app.controller.js
+++ js/app.controller.js
@@ -69,6 +69,7 @@ function renderLocs(locs) {
 }
 
 function onRemoveLoc(locId) {
+    if (!confirm('Do you want to remove location?')) return
     locService.remove(locId)
         .then(() => {
             flashMsg('Location removed')
